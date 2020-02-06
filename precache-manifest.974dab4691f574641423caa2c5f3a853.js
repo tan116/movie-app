e8867c1a2572dc8da433c17d007d08c996574c94
Tanay Sharda 11 <tanaysharda11@MacBook-Pro.local>
2020-02-06 17:34:41 +0530

    Updates

---
+++ precache-manifest.974dab4691f574641423caa2c5f3a853.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c6bdb161c84b18efe83d83efd7ddcaa",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "996fce10be2963fc0308",
+    "url": "/movie-app/static/css/main.5e1ba2db.chunk.css"
+  },
+  {
+    "revision": "457e2a737495218c322b",
+    "url": "/movie-app/static/js/2.412c2a3e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/movie-app/static/js/2.412c2a3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "996fce10be2963fc0308",
+    "url": "/movie-app/static/js/main.8052d4ad.chunk.js"
+  },
+  {
+    "revision": "60f14217498504fcfb6a",
+    "url": "/movie-app/static/js/runtime-main.8e14a1ed.js"
+  }
+]);
